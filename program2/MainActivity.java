8f39d94ad1073e52b6bc5613d7a6516fcfbf9f80
tanu017 <tanuiyengar2005@gmail.com>
2026-06-28 19:52:04 +0530

    program 2 and 3

--- program2/MainActivity.java
+++ program2/MainActivity.java
@@ -1,5 +1,12 @@
 package com.example.myapplication;
-import android.content.res.Configuration; import android.os.Bundle; import android.widget.TextView; import androidx.activity.EdgeToEdge; import androidx.appcompat.app.AppCompatActivity; import androidx.core.graphics.Insets; import androidx.core.view.ViewCompat; import androidx.core.view.WindowInsetsCompat;
+import android.content.res.Configuration;
+import android.os.Bundle; 
+import android.widget.TextView; 
+import androidx.activity.EdgeToEdge; 
+import androidx.appcompat.app.AppCompatActivity; 
+import androidx.core.graphics.Insets; 
+import androidx.core.view.ViewCompat; 
+import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
